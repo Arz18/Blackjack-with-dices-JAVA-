4b61cb16757924dc055fd560ebc659e83ce011d9
Arz18 <aaarivera18@gmail.com>
2024-12-09 23:30:49 -0400

    Update Dice.java
    Error de sintax corregido.

--- Dice.java
+++ Dice.java
@@ -3,15 +3,14 @@ import java.util.Random;
 // Clase que simula un dado de 14 caras utilizado en el juego.
 public class Dice {
     private Random random; // Objeto para generar números aleatorios.
-
+    
     // Constructor que inicializa el generador de números aleatorios.
     public Dice() {
         random = new Random();
-
+    }
     // Método que simula el lanzamiento del dado y devuelve un valor entre 1 y 14.
     public int roll() {
        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11}; //Valores de las instrucciones.
         return values[random.nextInt(values.length)]; // Selecciona un valor aleatorio del arreglo.
     }
 }
-
